0afb60e6567c9cc4a4c78ef17379594c4c4ae3f7
Sérgio Igreja <sergioedcigreja@gmail.com>
2021-08-29 17:11:05 +0100

    Initialization of project

---
+++ src/main/java/pt/sergioigreja/doggos/DoggosApplication.java
@@ -0,0 +1,13 @@
+package pt.sergioigreja.doggos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoggosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoggosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pt/sergioigreja/doggos/DoggosApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.sergioigreja.doggos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoggosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
